f5432964e90ccd055f3d74a410e703cddfe7a059
Benjaphon Niramit <benjaphon.work@gmail.com>
2019-09-25 18:56:10 +0700

    test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Deploy 101
                 </div>
 
                 <div class="links">
